0a5758fa278481c83910ad61de1b9baa1d396ccf
joshliford <j_liford1@yahoo.com>
2025-10-19 15:07:05 -0500

    added styling to the entire application

--- practice-react-app/src/components/ProfileCard.jsx
+++ practice-react-app/src/components/ProfileCard.jsx
@@ -1,9 +1,11 @@
+import './ProfileCard.css';
+
 const ProfileCard = ( {name, age, bio} ) => {
     return (
         <div className="profile-card">
-            <h1>Name: {name}</h1>
-            <p>Age: {age}</p>
-            <p>Bio: {bio}</p>
+            <h1 className="profile-name"><strong>Name:</strong> {name}</h1>
+            <p className="profile-age"><strong>Age:</strong> {age}</p>
+            <p className="profile-bio"><strong>Bio:</strong> {bio}</p>
         </div>
     )
 }
--- practice-react-app/src/components/ProfileContainer.jsx
+++ practice-react-app/src/components/ProfileContainer.jsx
@@ -1,23 +1,28 @@
+import './ProfileContainer.css'
 import ProfileCard from "./ProfileCard"
 
 const ProfileContainer = () => {
     return (
         <div className="profile-container">
-            <ProfileCard
-                name="Josh L"
-                age={28}
-                bio="Jr. Applications Developer who is passionate about AI and enjoys playing the guitar."
-            />
-            <ProfileCard
-                name="David G"
-                age={78}
-                bio="Legendary guitarist and vocalist of Pink Floyd."
-            />
-            <ProfileCard
-                name="Jerry C"
-                age={58}
-                bio="Incredible guitarist, vocalist, and songwriter for Alice in Chains."
-            />
+            <header className="container-header">User Profiles</header>
+            <hr></hr>
+                <ProfileCard
+                    name="Josh L"
+                    age={28}
+                    bio="Jr. Applications Developer who is passionate about AI and enjoys playing the guitar."
+                />
+                <ProfileCard
+                    name="David G"
+                    age={78}
+                    bio="Legendary guitarist and vocalist of Pink Floyd."
+                />
+                <ProfileCard
+                    name="Jerry C"
+                    age={58}
+                    bio="Incredible guitarist, vocalist, and songwriter for Alice in Chains."
+                />
+            <hr></hr>
+            <footer className="footer">&copy; 2025 Josh Liford</footer>
         </div>
     )
 }
